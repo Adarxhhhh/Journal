7ea5722b056e8a46f505671db1501fc6f1e9ef3f
Adarxhhhh <katigiadarsh@gmail.com>
2026-02-02 22:24:35 -0600

    Added Lombok. The issue was lombok mismatching with java version. Project was running java25 and lombok was running java 17, update java version to java17@temurin and works fine now. Getters and setters are generated normally now

--- src/main/java/net/engineeringdigest/journalApplication/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApplication/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package net.engineeringdigest.journalApplication.entity;
 
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,7 +8,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JournalEntry {
 
     @Id
@@ -19,36 +22,4 @@ public class JournalEntry {
 
     private LocalDateTime date;
 
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
